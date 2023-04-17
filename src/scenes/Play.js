06713ba49bd0e246d9ad5a4bbf1066762ea45712
Matthew Guo <mjguo@ucsc.edu>
2023-04-17 11:50:10 -0700

    first commit

---
+++ src/scenes/Play.js
@@ -0,0 +1,12 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    create() {
+        // green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        // white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
+    }
+}
