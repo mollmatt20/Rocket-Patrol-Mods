97e425d01f5658104d5fd2e53616ddb1541aeec3
Matthew Guo <mjguo@ucsc.edu>
2023-04-25 22:34:34 -0700

    added new spaceship

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,6 +7,7 @@ class Play extends Phaser.Scene {
         //load images/tile sprites
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('ufo', './assets/UFO.png');
         this.load.image('starfield', './assets/starfield.png');
         this.load.image('astral field', './assets/Astral Field.png');
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64,
@@ -27,10 +28,13 @@ class Play extends Phaser.Scene {
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
-        // add spaceships (x3)
+        // add spaceships (x4)
+        this.ship00 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*6, 'ufo', 0, 60).setOrigin(0, 0);
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0, 0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0);
+        // set ufo move speed as fastest one
+        this.ship00.moveSpeed += 2;
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
@@ -44,7 +48,8 @@ class Play extends Phaser.Scene {
         });
         // initialize score
         this.p1Score = 0;
-    
+        // initialize high score
+        //this.p1HighScore = 0;
         // display score
         let scoreConfig = {
             fontFamily: 'Courier',
@@ -74,6 +79,7 @@ class Play extends Phaser.Scene {
         }
 
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+        this.highScore = this.add.text(borderUISize + borderPadding*30, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
         // Included FIRE text in UI
         this.fireText = this.add.text(borderUISize + borderPadding*15, borderUISize + borderPadding*2, 'FIRE', fireConfig);
         // GAME OVER flag
@@ -90,16 +96,12 @@ class Play extends Phaser.Scene {
 
         // 30-second ship speed up
         this.speedUp = this.time.delayedCall(game.settings.gameTimer/2, () => {
+            this.ship00.moveSpeed += 2;
             this.ship01.moveSpeed += 2;
             this.ship02.moveSpeed += 2;
             this.ship03.moveSpeed += 2;
             this.p1Rocket.moveSpeed += 2;
         }, null, this);
-
-        // stop music
-        if (this.gameOver) {
-            this.sound.stop('music_background');
-        }
     }   
 
     update() {
@@ -113,6 +115,7 @@ class Play extends Phaser.Scene {
         this.starfield.tilePositionX -= 4;
         if(!this.gameOver) {
             this.p1Rocket.update();
+            this.ship00.update();
             this.ship01.update();
             this.ship02.update();
             this.ship03.update();
@@ -129,6 +132,10 @@ class Play extends Phaser.Scene {
         if (this.checkCollision(this.p1Rocket, this.ship01)) {
             this.p1Rocket.reset();
             this.shipExplode(this.ship01);
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship00)) {
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship00);
         }
     }
 
